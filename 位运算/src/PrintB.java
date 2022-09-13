a58f46a94e4b847e3e8f1996846abc5ad4458ce7
任玉双 <renyushuang@meituan.com>
2022-09-13 13:59:27 +0800

    增

--- 位运算/src/PrintB.java
+++ 位运算/src/PrintB.java
@@ -1,5 +1,11 @@
 /**
  * @author renyushuang 打印二进制
+ * 
+ * 
+ * 一个数左移动，那么当前数乘二
+ * 
+ * 整型的最高位是 用来判断 + - 
+ * 
  */
 public class PrintB {
 
@@ -15,6 +21,18 @@ public class PrintB {
 
 		int num = 127148343;
 		print(num);
+		
+		
+		//11111111111111111111111111111111
+		//1负数 不管了 后面去反+1
+		print(-1);
+		
+		int c = 1212;
+		int b = ~c;
+		print(c);
+		print(b);
+		
+		
 
 	}
 }
