d93c6fda7fd02254559230d983f66ffe98196678
任玉双 <renyushuang@meituan.com>
2022-09-29 20:15:46 +0800

    链表

---
+++ 链表/src/链表/LinkListSum.java
@@ -0,0 +1,19 @@
+package 链表;
+
+/**
+ * @author renyushuang
+ * 
+ *         给定两个链表的头节点heard1，heard2 认为从左到右是某个数字从低位到高位，返回相加之后的链表 4 -> 3 -> 6 2 ->
+ *         5 -> 3
+ * 
+ *         6 -> 8 -> 9
+ * 
+ *         解释643+352 = 986
+ */
+public class LinkListSum {
+
+	public static void main(String[] args) {
+
+	}
+
+}
