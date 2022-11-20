ead176c4c89c24444fdeb9e822a335e65501925b
任玉双 <renyushuang@meituan.com>
2022-11-20 23:24:50 +0800

    LC026

---
+++ LeetCode/src/com/renyushuang/LC025__.java
@@ -0,0 +1,62 @@
+
+package com.renyushuang;
+
+import com.common.ListNode;
+
+/*
+*	作者：renyushuang
+*	日期：2022年11月20日下午10:17:45
+* 
+* 	题目：25. K 个一组翻转链表
+* 	
+	给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+	
+	k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+	
+	你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+	
+	 
+	
+	示例 1：
+	https://leetcode.cn/problems/reverse-nodes-in-k-group/
+	
+	输入：head = [1,2,3,4,5], k = 2
+	输出：[2,1,4,3,5]
+	示例 2：
+	
+	
+	
+	输入：head = [1,2,3,4,5], k = 3
+	输出：[3,2,1,4,5]
+	 
+	
+	提示：
+	链表中的节点数目为 n
+	1 <= k <= n <= 5000
+	0 <= Node.val <= 1000
+	 
+	
+	进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗
+	
+
+*/
+public class LC025__ {
+
+	public static void main(String[] args) {
+		int[] aar = { 1, 2, 3, 4, 5 };
+
+		ListNode aa = ListNode.createNodeList(aar);
+		reverseKGroup(aa, 2);
+
+	}
+
+	public static ListNode reverseKGroup(ListNode head, int k) {
+
+		ListNode temp = new ListNode(-1);
+	
+
+		return temp.next;
+
+	}
+
+}
