b90ee4d4156eef0f556cd54ff1973fde474b13a6
任玉双 <renyushuang@meituan.com>
2022-11-28 21:43:32 +0800

    aaa

---
+++ LeetCode/src/com/renyushuang/LC029_.java
@@ -0,0 +1,59 @@
+
+package com.renyushuang;
+
+/*
+*	作者：renyushuang
+*	日期：2022年11月22日上午11:27:22
+* 
+* 	题目：29. 两数相除
+* 	
+	给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+	
+	返回被除数 dividend 除以除数 divisor 得到的商。
+	
+	整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+	
+	 
+	
+	示例 1:
+	
+	输入: dividend = 10, divisor = 3
+	输出: 3
+	解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+	示例 2:
+	
+	输入: dividend = 7, divisor = -3
+	输出: -2
+	解释: 7/-3 = truncate(-2.33333..) = -2
+	 
+	
+	提示：
+	
+	被除数和除数均为 32 位有符号整数。
+	除数不为 0。
+	假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
+
+
+*/
+public class LC029_ {
+
+	public static void main(String[] args) {
+		System.out.println("" + divide(10, 3));
+	}
+
+	public static int divide(int dividend, int divisor) {
+		if (dividend == 0) {
+			return 0;
+		}
+		if (dividend == Integer.MIN_VALUE && divisor == -1) {
+			return Integer.MAX_VALUE;
+		}
+
+		// 100 3 = 33
+		// 100 / 2^32 > 3
+		// 100 / 2^5 >=3 ->res+2^5,100-3*2^5 = 4
+		// 4 / 2^0 >=3 ->res+2^0,4-3*2^0=1
+
+		return 0;
+	}
+}
---
+++ LeetCode/src/com/renyushuang/LC032.java
@@ -0,0 +1,31 @@
+
+package com.renyushuang;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+*	作者：renyushuang
+*	日期：2022年11月23日下午8:49:07
+* 
+* 	题目：
+* 	
+* 	示例：
+*/
+public class LC032 {
+
+	public static void main(String[] args) {
+		System.out.println("" + longestValidParentheses("(()"));
+		System.out.println("" + longestValidParentheses(")()())"));
+//		System.out.println("" + longestValidParentheses(""));
+		System.out.println("" + longestValidParentheses("()(()"));
+
+	}
+
+	public static int longestValidParentheses(String s) {
+
+
+		return res;
+
+	}
+}
